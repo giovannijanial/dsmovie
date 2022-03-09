938d5c7b14bc79b26929102e69be10ed2c2c1718
Giovanni Janial <giovannijanial@gmail.com>
2022-03-08 21:40:44 -0300

    routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,23 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from "./pages/listing";
+import Form from './pages/form';
 import Navbar from "./components/navbar";
-
 
 function App() {
   return (
-    <Navbar />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
