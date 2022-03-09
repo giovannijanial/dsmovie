c3a3ba7233a00cc1425e742480595eb864b3bc1e
Giovanni Janial <giovannijanial@gmail.com>
2022-03-09 14:05:02 -0300

    mudança navbar

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -10,7 +10,7 @@ function Navbar() {
                     <a href="https://github.com/giovannijanial">
                         <div className='dsmovie-contact-container'>
                             <GithubIcon />
-                            <p className='dsmovie-contact-link'>/devsuperior</p>
+                            <p className='dsmovie-contact-link'>/giovannijanial</p>
                         </div>
                     </a>
                 </div>
